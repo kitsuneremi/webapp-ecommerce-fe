1e94b9034908898452970ed812b6c237faebca46
lily <89727355+kitsuneremi@users.noreply.github.com>
2024-02-21 20:53:55 +0700

    prômtion

--- admin/src/pages/promotion/index.tsx
+++ admin/src/pages/promotion/index.tsx
@@ -1,13 +1,15 @@
 import { Input, Button, Space, Table, Tag, Form, Checkbox, DatePicker } from 'antd/lib';
 import type { TableProps } from 'antd/lib';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import axios from 'axios';
 import { baseUrl } from '../../lib/functional';
 import { useToast } from '@/components/ui/use-toast';
 import { Layout as DashboardLayout } from '../../layouts/dashboard/layout';
-
+import { makeid } from '../../lib/functional';
+import { redirect, usePathname, useSearchParams } from 'next/navigation';
 interface DataType {
+    id: number;
     code: string;
     name: string;
     value: number;
@@ -21,7 +23,18 @@ type FieldType = {
     name?: string;
     quantity?: string;
     value?: string;
-    date: [Dayjs, Dayjs],
+    description: string;
+    date: [Dayjs, Dayjs]
+};
+
+type UpdateFieldType = {
+    id: number;
+    code: string;
+    name?: string;
+    quantity?: string;
+    value?: string;
+    description: string;
+    date: [Dayjs, Dayjs]
 };
 
 const { RangePicker } = DatePicker
@@ -75,6 +88,7 @@ const columns: TableProps<DataType>['columns'] = [
 
 const data: DataType[] = [
     {
+        id: 0,
         code: '1',
         name: "Furina's bath water",
         value: 10,
@@ -84,6 +98,7 @@ const data: DataType[] = [
         endDate: new Date()
     },
     {
+        id: 1,
         code: '2',
         name: 'aloooo',
         value: 10,
@@ -93,6 +108,7 @@ const data: DataType[] = [
         endDate: new Date()
     },
     {
+        id: 2,
         code: '3',
         name: 'oke',
         value: 10,
@@ -110,24 +126,64 @@ const onFinishFailed = (errorInfo: any) => {
 
 
 
-const PromotionPage = (): JSX.Element => {
+const PromotionPage = (props): JSX.Element => {
 
     const { toast } = useToast();
 
+    const path = useSearchParams();
     const [panel, setPanel] = useState<number>(0);
     const [date, setDate] = useState<[Dayjs, Dayjs]>();
+    const [targetPromotion, setTargetPromotion] = useState<DataType>()
+
+    useEffect(() => {
+        if (path.get("id")) {
+            axios.get(`${baseUrl}/promotion/${path.get("id")}`).then(res => {
+                setTargetPromotion(res.data)
+            })
+        }
+    }, [path])
 
     const handleSubmitForm = (values: any) => {
-
         if (date) {
             const data = {
-
+                code: makeid(),
+                name: values.name,
+                description: values.description,
+                quantity: values.quantity,
+                startDate: dayjs(values.date[0]).toDate(),
+                endDate: dayjs(values.date[1]).toDate(),
             }
             axios.post(`${baseUrl}/promotion`, data).then(res => {
                 toast({
                     title: res.data.title,
                     description: res.data.des
                 })
+                if (res.data.status == "Success") {
+                    redirect(`/promotion/${res.data.data.id}`)
+                }
+            })
+        }
+    }
+
+    const handleUpdateForm = (values: any) => {
+        if (date) {
+            const data = {
+                id: values.id,
+                code: values.code,
+                name: values.name,
+                description: values.description,
+                quantity: values.quantity,
+                startDate: dayjs(values.date[0]).toDate(),
+                endDate: dayjs(values.date[1]).toDate(),
+            }
+            axios.put(`${baseUrl}/promotion/${values.id}`, data).then(res => {
+                toast({
+                    title: res.data.title,
+                    description: res.data.des
+                })
+                if (res.data.status == "Success") {
+                    redirect(`/promotion/${res.data.data.id}`)
+                }
             })
         }
     }
@@ -147,6 +203,76 @@ const PromotionPage = (): JSX.Element => {
                 <div>
                     {panel == 0 &&
                         <>
+                            {targetPromotion && <div className='mb-2 px-3 py-2'>
+                                <Form
+                                    name="basic"
+                                    labelCol={{ span: 8 }}
+                                    wrapperCol={{ span: 16 }}
+                                    style={{ maxWidth: 600 }}
+                                    initialValues={{ remember: true }}
+                                    onFinish={handleUpdateForm}
+                                    // onFinishFailed={onFinishFailed}
+                                    autoComplete="off"
+                                >
+                                    <Form.Item<UpdateFieldType>
+                                        label="id"
+                                        name="name"
+                                    >
+                                        <Input disabled value={targetPromotion.id} />
+                                    </Form.Item>
+                                    <Form.Item<UpdateFieldType>
+                                        label="code"
+                                        name="code"
+                                    >
+                                        <Input disabled value={targetPromotion.code} />
+                                    </Form.Item>
+                                    <Form.Item<UpdateFieldType>
+                                        label="tên chương trình"
+                                        name="name"
+                                        rules={[{ required: true, message: 'Hãy nhập tên chương trình' }]}
+                                    >
+                                        <Input value={targetPromotion.name} />
+                                    </Form.Item>
+
+                                    <Form.Item<UpdateFieldType>
+                                        label="giá trị giảm"
+                                        name="value"
+                                        rules={[{ required: true, message: 'Hãy nhập giá trị giảm' }]}
+                                    >
+                                        <Input value={targetPromotion.value} />
+                                    </Form.Item>
+
+
+                                    <Form.Item<UpdateFieldType>
+                                        label="số lượng"
+                                        name="quantity"
+                                        rules={[{ required: true, message: 'Hãy nhập số lượng' }]}
+                                    >
+                                        <Input value={targetPromotion.quantity} />
+                                    </Form.Item>
+
+                                    <Form.Item<UpdateFieldType>
+                                        label="mô tả"
+                                        name="description"
+                                    >
+                                        <Input.TextArea value={targetPromotion.description} />
+                                    </Form.Item>
+
+                                    <Form.Item<UpdateFieldType>
+                                        label="ngày bắt đầu - kết thúc"
+                                        name="date"
+                                        rules={[{ required: true, message: 'Hãy nhập ngày tháng' }]}
+                                    >
+                                        <RangePicker value={[dayjs(targetPromotion.startDate), dayjs(targetPromotion.endDate)]} onChange={(val) => { setDate(val) }} showTime />
+                                    </Form.Item>
+
+                                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                                        <Button type="primary" className='bg-blue-500' htmlType="submit">
+                                            Submit
+                                        </Button>
+                                    </Form.Item>
+                                </Form>
+                            </div>}
                             <div className="flex gap-5">
                                 <Input placeholder="Basic usage" />
                             </div>
@@ -194,6 +320,13 @@ const PromotionPage = (): JSX.Element => {
                                 </Form.Item>
 
                                 <Form.Item<FieldType>
+                                    label="mô tả"
+                                    name="description"
+                                >
+                                    <Input.TextArea />
+                                </Form.Item>
+
+                                <Form.Item<FieldType>
                                     label="ngày bắt đầu - kết thúc"
                                     name="date"
                                     rules={[{ required: true, message: 'Hãy nhập ngày tháng' }]}
@@ -202,7 +335,7 @@ const PromotionPage = (): JSX.Element => {
                                 </Form.Item>
 
                                 <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                                    <Button type="primary" htmlType="submit">
+                                    <Button type="primary" className='bg-blue-500' htmlType="submit">
                                         Submit
                                     </Button>
                                 </Form.Item>
